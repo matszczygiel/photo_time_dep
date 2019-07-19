4ab2a2fb2048b0f03f929e5aa798a9e314823501
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-07-19 15:45:47 +0200

    plot.py script updated

--- deploy/plot.py
+++ deploy/plot.py
@@ -11,8 +11,8 @@ file_vel = "res_vel.out"
 data_len = np.loadtxt(file_len, skiprows=17)
 data_vel = np.loadtxt(file_vel, skiprows=17)
 
-plt.plot(data_len[:, 0], data_len[:, 3], label="length")
-plt.plot(data_vel[:, 0], data_vel[:, 3], label="velocity")
+plt.plot(data_len[:, 0], data_len[:, 3], linewidth=0.7, color='b', label="length")
+plt.plot(data_vel[:, 0], data_vel[:, 3], linewidth=0.7, color='r', label="velocity")
 
 plt.title(name)
 plt.legend()
