e9f2249f3bd02b8b784ee1c1bc3860072610c4dd
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-08-07 13:44:05 +0200

    plot resolution change

--- deploy/plot.py
+++ deploy/plot.py
@@ -8,6 +8,8 @@ name = sys.argv[1]
 file_len = "res_len.out"
 file_vel = "res_vel.out"
 
+resolution=400
+
 data_len = np.loadtxt(file_len, skiprows=21)
 data_vel = np.loadtxt(file_vel, skiprows=21)
 
@@ -17,7 +19,7 @@ plt.plot(data_vel[:, 0], data_vel[:, 3], linewidth=0.7, color='r', label="veloci
 plt.title(name)
 plt.grid()
 plt.legend()
-plt.savefig("dip.png")
+plt.savefig("dip.png", dpi=resolution)
 
 plt.clf()
 plt.plot(data_len[:, 0], data_len[:, 4], linewidth=0.7, color='b', label="length")
@@ -25,7 +27,7 @@ plt.plot(data_vel[:, 0], data_vel[:, 4], linewidth=0.7, color='r', label="veloci
 plt.title(name)
 plt.grid()
 plt.legend()
-plt.savefig("norm.png")
+plt.savefig("norm.png", dpi=resolution)
 
 plt.clf()
 plt.plot(data_len[:, 0], data_len[:, 5], linewidth=0.7, color='b', label="length")
@@ -33,4 +35,4 @@ plt.plot(data_vel[:, 0], data_vel[:, 5], linewidth=0.7, color='r', label="veloci
 plt.title(name)
 plt.grid()
 plt.legend()
-plt.savefig("energy.png")
+plt.savefig("energy.png", dpi=resolution)
